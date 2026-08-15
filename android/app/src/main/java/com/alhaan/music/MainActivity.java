ff2b00c3c824ddb6d4d5821c2352c877a27dac9d
Aboode2009 <barra2234barra@gmail.com>
2026-08-15 07:34:23 +0300

    Register native media player plugin and notification permission

--- android/app/src/main/java/com/alhaan/music/MainActivity.java
+++ android/app/src/main/java/com/alhaan/music/MainActivity.java
@@ -1,11 +1,19 @@
 package com.alhaan.music;
 
+import android.Manifest;
+import android.os.Build;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         registerPlugin(YoutubeDownloaderPlugin.class);
+        registerPlugin(MediaPlayerPlugin.class);
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 7001);
+        }
     }
 }

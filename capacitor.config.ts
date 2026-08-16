2068fd67e9411ce4830c8fedce9e83cf57458906
Aboode2009 <barra2234barra@gmail.com>
2026-08-16 10:28:46 +0300

    Set Capacitor app name consistently to Alhan

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.alhaan.music',
-  appName: 'youtube-tarab-player',
+  appName: 'Alhan',
   webDir: 'dist',
   server: {
     androidScheme: 'https'

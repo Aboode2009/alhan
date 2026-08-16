7a68652bfece56ba1d565bb979bad552ef9b90e1
Aboode2009 <barra2234barra@gmail.com>
2026-08-16 11:46:19 +0300

    Use uploaded Alhan MainActivity

--- android/app/src/main/java/com/alhaan/music/MainActivity.java
+++ android/app/src/main/java/com/alhaan/music/MainActivity.java
@@ -2,6 +2,7 @@ package com.alhaan.music;
 
 import android.os.Bundle;
 import android.util.Log;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -9,9 +10,20 @@ public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        try { registerPlugin(YoutubeDownloaderPlugin.class); } catch (Throwable t) { Log.e(TAG, "YoutubeDownloader registration failed", t); }
-        try { registerPlugin(MediaPlayerPlugin.class); } catch (Throwable t) { Log.e(TAG, "MediaPlayer registration failed", t); }
-        try { registerPlugin(YoutubeSearchPlugin.class); } catch (Throwable t) { Log.e(TAG, "YoutubeSearch registration failed", t); }
+        // Capacitor requires custom plugins to be registered before super.onCreate().
+        // Keep each registration isolated so one optional native feature can never
+        // prevent the main Alhan UI from starting.
+        try {
+            registerPlugin(YoutubeDownloaderPlugin.class);
+        } catch (Throwable t) {
+            Log.e(TAG, "YoutubeDownloader plugin registration failed", t);
+        }
+        try {
+            registerPlugin(MediaPlayerPlugin.class);
+        } catch (Throwable t) {
+            Log.e(TAG, "MediaPlayer plugin registration failed", t);
+        }
+
         super.onCreate(savedInstanceState);
     }
 }
